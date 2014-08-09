0f56b68ad3cdee3ca850f9f1f01822cd549ca56d
Martin Keegan <martin@no.ucant.org>
2014-08-10 00:03:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Example iron app
+================
+
+To use:
+
+1. Ensure toolchain is up-to-date
+2. Build
+3. Run and test
+
+Keeping up-to-date
+------------------
+
+You will need to be up-to-date with Rust and Cargo. These tools change
+daily, and 
+
+Building
+--------
+
+    cargo build
+    cargo run
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate http;
+extern crate iron;
+
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, Chain, Request, Response, Server, Status, Unwind, FromFn};
+
+fn hello_world(_req: &mut Request, res: &mut Response) -> Status {
+    let _ = res.serve(::http::status::Ok, "Hello, world!\n");
+    Unwind
+}
+
+fn main() {
+    let mut server: Server = Iron::new();
+    server.chain.link(FromFn::new(hello_world));
+    server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
+}
